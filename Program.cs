fc05223d81d09c9ef681bc75218358928cfd1473
Vicky Le <vle04@student.rtc.edu>
2024-04-20 15:26:07 -0700

    Update with adding name, class, date, and assignment

--- Program.cs
+++ Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Name: Vicky Le
+            // Class: Programming 120
+            // Date: 4/20/24
+            // Code Practice - Booleans and Conditions Assignments
+
         }
     }
 }
