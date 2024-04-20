a1ff14326468396c1a3ebeebcee69e051d563126
Vicky Le <vle04@student.rtc.edu>
2024-04-20 15:24:57 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Code_Practice___Booleans_and_Conditions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
